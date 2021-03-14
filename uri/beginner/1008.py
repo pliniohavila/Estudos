7373dc1d35d4d85fc51d9473ab9136a3849d86ea
pliniocode <76018725+pliniocode@users.noreply.github.com>
2021-03-14 12:34:16 -0300

    Create 1008.py

---
+++ uri/beginner/1008.py
@@ -0,0 +1,9 @@
+numberEmployer = int(input())
+hour = int(input())
+perHour = float(input())
+
+salary = hour * perHour
+
+print("NUMBER = {}".format(numberEmployer))
+print("SALARY = U$ {:.2f}".format(salary))
+
