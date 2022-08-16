aa5fd18c58b1d2513ee6208eafd3685452f62a49
Plínio Ribeiro <76018725+pliniocode@users.noreply.github.com>
2022-08-16 20:28:31 -0300

    Add files via upload

---
+++ React/C:\Users\Win10\DevStudys\React\Reac-Hook-Form-Code-Post/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+import { Form } from './Form';
+
+export default function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
